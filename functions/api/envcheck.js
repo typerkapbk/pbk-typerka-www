46728069adb139270364a58a48b33d3c55564383
typerkapbk <typerkapbk@gmail.com>
2026-08-25 19:36:19 +0200

    Add envcheck API endpoint for environment variables

---
+++ functions/api/envcheck.js
@@ -0,0 +1,12 @@
+export async function onRequestGet(context) {
+  return Response.json({
+    TEAM_DOMAIN: !!context.env.TEAM_DOMAIN,
+    POLICY_AUD: !!context.env.POLICY_AUD,
+    ADMIN_EMAIL: !!context.env.ADMIN_EMAIL,
+    GOOGLE_SHEET_ID: !!context.env.GOOGLE_SHEET_ID,
+    GOOGLE_SERVICE_ACCOUNT_EMAIL:
+      !!context.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
+    GOOGLE_PRIVATE_KEY:
+      !!context.env.GOOGLE_PRIVATE_KEY
+  });
+}
