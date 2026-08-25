44c76c5aec2737be6984c9e55acea5bf2c11a9d7
typerkapbk <typerkapbk@gmail.com>
2026-08-25 18:36:57 +0200

    Enhance getSheetRange with formatted value option
    Added formatted value option to getSheetRange for WWW_AKTUALNOSCI.

--- functions/api/data.js
+++ functions/api/data.js
@@ -154,7 +154,12 @@ export async function onRequestGet(context) {
   getSheetRange(context.env, token, "'TABELA KOLEJKI'!A1:H20"),
   getSheetRange(context.env, token, "'M&S'!A1:AT30"),
   getSheetRange(context.env, token, "TT!A1:BG80"),
-  getSheetRange(context.env, token, "'WWW_AKTUALNOSCI'!A1:F200")
+  getSheetRange(
+  context.env,
+  token,
+  "'WWW_AKTUALNOSCI'!A1:F200",
+  "FORMATTED_VALUE"
+)
 ]);
 
     return Response.json({
